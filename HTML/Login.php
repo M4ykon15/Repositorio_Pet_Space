84b8020d98e4c5630cb4d2d94597f6053e4abc11
kryspdf <caiosan09@gmail.com>
2023-08-26 20:52:28 -0300

    first commit

--- HTML/Login.php
+++ HTML/Login.php
@@ -4,9 +4,15 @@
     <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Pet Space</title>
+
+  
+
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+        
         <link rel="stylesheet" href="../CSS/Login.css">
-        <title>Pet Space</title>
+     
         <link rel="shortcut icon" href="../Imagens/icon_pata.ico" type="image/x-icon">
        
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
